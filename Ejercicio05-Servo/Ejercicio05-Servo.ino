a8384929eb41cdf2697f11f1f5a96d1c70125f80
Enrique Crespo <jecrespom@gmail.com>
2019-09-03 18:31:35 +0200

    ejercicio05

---
+++ Ejercicio05-Servo/Ejercicio05-Servo.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+#define PIN_SERVO 9
+#define PIN_POT A0
+
+Servo miservo;
+
+void setup() {
+  miservo.attach(PIN_SERVO);
+}
+
+void loop() {
+  int val = analogRead(PIN_POT);
+  val = map(val, 0, 1023, 10, 170);
+  miservo.write(val);
+  delay(15);
+}
