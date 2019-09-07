3a81bc2228daac3a035803ce00a3eba99d5250be
Enrique Crespo <jecrespom@gmail.com>
2019-09-07 10:02:03 +0200

    Ejercicio06 con web server

--- Ejercicio06-Cuenta_Pulsos_Wifi/Ejercicio06-Cuenta_Pulsos_Wifi.ino
+++ Ejercicio06-Cuenta_Pulsos_Wifi/Ejercicio06-Cuenta_Pulsos_Wifi.ino
@@ -1,3 +1,13 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+/* Set these to your desired credentials. */
+const char *ssid = "ESPapXXXX";
+const char *password = "passwordXXXX";
+
+ESP8266WebServer server(80);
+
 //Librería Pantalla
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
@@ -25,9 +35,22 @@ void setup() {
 
   //Pantalla
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
+
+  Serial.print("Configuring access point...");
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
+  server.handleClient();
+
   boolean estado_b = digitalRead(PIN_BOTON_B);
 
   if (anterior_b != estado_b) {
@@ -35,7 +58,7 @@ void loop() {
       Serial.println(F("Pulsado boton B"));
       pulsos_b++;
       Serial.println("Num. pulsaciones: " + (String)pulsos_b);
-      int tiempo = (millis()-tiempo_pulsos);
+      int tiempo = (millis() - tiempo_pulsos);
       tiempo_pulsos = millis();
       Serial.println("Tiempo entre pulsos: " + (String)tiempo + " ms");
 
@@ -55,4 +78,13 @@ void loop() {
   }
   anterior_b = estado_b;
   delay(50); //Evitar rebotes
+}
+
+void handleRoot() {
+  String web = "<!DOCTYPE html>\n<html>\n<head><meta http-equiv=\"refresh\" content=\"60\" /></head>\n";
+  web += "<body>\n";
+  web += "<h1>Pulsaciones: </h1>" + String(pulsos_b) + "<h1>Tiempo entre pulsos: </h1>" + String(tiempo_pulsos);
+  web += "</body>\n</html>\n";
+  server.send(200, "text/html", web);
+  server.sendHeader("refresh","60");
 }
